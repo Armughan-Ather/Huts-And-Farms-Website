e258c23168acb4d7e38172ca2551362ee3769479
Armughan-Ather <armughan.ather83@gmail.com>
2025-10-13 19:26:58 +0500

    imports update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
-import UserLogin from "./pages/UserLogin/UserLogin";
-import UserSignup from "./pages/UserSignup/UserSignup";
-import UserChat from "./pages/UserChat/UserChat";
+import UserLogin from "./pages/UserLogin/UserLogin.jsx";
+import UserSignup from "./pages/UserSignup/UserSignup.jsx";
+import UserChat from "./pages/UserChat/UserChat.jsx";
 
-import AdminLogin from "./pages/Login/Login";
-import Dashboard from "./pages/dashboard/dashboard";
-import Bookings from "./pages/bookings/bookings";
-import AddBooking from "./pages/add-booking/add-booking";
+import AdminLogin from "./pages/Login/login.jsx";
+import Dashboard from "./pages/dashboard/dashboard.jsx";
+import Bookings from "./pages/bookings/bookings.jsx";
+import AddBooking from "./pages/add-booking/add-booking.jsx";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 import {jwtDecode} from "jwt-decode"; // ✅ Needed for token validation
